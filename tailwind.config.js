ee37ef4c5fb69d7fdddcc9c96a01e54a78cf2ec9
Yasin Taghavi <mrtqvi@yahoo.com>
2023-01-14 02:01:24 +0330

    admin panel v2

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: 'class',
+  mode: 'jit',
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        "sm" : "641px",
+        "xs" : "480px"
+      },
+      fontFamily : {
+        'shabnam' :  ["shabnam"]
+      }
+    },
+  },
+  plugins: [],
+}
